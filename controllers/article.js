10f1322c29c6445ce29378d3cf6fa60aa536ef41
han-hum <hanna.hummal@voco.ee>
2024-09-13 08:56:43 +0300

    added public files, err fix

--- controllers/article.js
+++ controllers/article.js
@@ -2,19 +2,20 @@ const articleDbModel = require('../models/article')
 const articleModel = new articleDbModel()
 
 class articleController {
-    constructor(){
-        const articles = [] 
-    } 
+    constructor() {
+        const articles = []
+    }
 
     async getAllArticles(req, res) {
         this.articles = await articleModel.findAll()
-        res.status(201).json({articles: this.articles})
-    }   
-} 
+        res.status(201).json({ articles: this.articles })
+    }
+
     async getArticleBySlug(reg, res) {
         const article = await articleModel.findOne(req.params.slug)
-        res.status(201).json({article: article})
+        res.status(201).json({ article: article })
     }
+}
 
 
 module.exports = new articleController()
